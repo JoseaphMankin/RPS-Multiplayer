ecdcc5cd6841ef018a55539bd27c3c9d100559bf
Joe Mankin <joseaphmankin@gmail.com>
2018-09-07 20:16:06 -0500

    Scoreboard wiring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,50 @@ $(document).ready(function () {
     let userScore = 0;
     let computerScore = 0;
     let userGuess = "";
+    let computerGuess="";
+    
 
     $(".choice").on("click", function () {
         userGuess = $(this).attr("data-type");
-        console.log(userGuess);
+        getComputerChoice();
+        console.log("Your Guess: " + userGuess);
+        console.log("Comp Guess: " + computerGuess);
+
+        if(userGuess === "rock" && computerGuess === "scissors"){
+            console.log("You Win");
+            userScore++;
+            $(".userScore").text(userScore);
+        } else if(userGuess === "rock" && computerGuess === "paper"){
+            console.log("You Lose");
+            computerScore++;
+            $(".computerScore").text(computerScore);
+        } else if(userGuess === "paper" && computerGuess === "rock"){
+            console.log("You Win");
+            userScore++;
+            $(".userScore").text(userScore);
+        } else if(userGuess === "paper" && computerGuess === "scissors"){
+            console.log("You Lose");
+            computerScore++;
+            $(".computerScore").text(computerScore);
+        } else if(userGuess === "scissors" && computerGuess === "paper"){
+            console.log("You Win");
+            userScore++;
+            $(".userScore").text(userScore);
+        } else if(userGuess === "scissors" && computerGuess === "rock"){
+            console.log("You Lose");
+            computerScore++;
+            $(".computerScore").text(computerScore);
+        } else {
+                console.log("It's a Draw");
+        }
+
     });
+
+    function getComputerChoice(){
+        const choices = ["rock", "paper", "scissors"];
+        let randomNumber = Math.floor(Math.random()*3);
+        computerGuess = choices[randomNumber];
+    }
 
 
 });
