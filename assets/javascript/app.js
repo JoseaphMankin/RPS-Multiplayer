ac0210b19e6f278adff7e6c7d85267180c13ac44
Joe Mankin <joseaphmankin@gmail.com>
2018-09-08 12:32:24 -0500

    Main game working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,16 @@ $(document).ready(function () {
     let userScore = 0;
     let computerScore = 0;
     let userGuess = "";
-    let computerGuess="";
-    
+    let computerGuess = "";
+
+    const leftRock = "assets/images/rock.png";
+    const leftPaper = "assets/images/paper.png";
+    const leftScissors = "assets/images/scissors.png";
+    const rightRock = "assets/images/rockRight.png";
+    const rightPaper = "assets/images/paperRight.png";
+    const rightScissors = "assets/images/scissorsRight.png";
+
+
 
     $(".choice").on("click", function () {
         userGuess = $(this).attr("data-type");
@@ -12,41 +20,65 @@ $(document).ready(function () {
         console.log("Your Guess: " + userGuess);
         console.log("Comp Guess: " + computerGuess);
 
-        if(userGuess === "rock" && computerGuess === "scissors"){
-            console.log("You Win");
+        if (userGuess === "rock" && computerGuess === "scissors") {
+            $(".pickLeft").attr("src", leftRock);
+            $(".pickRight").attr("src", rightScissors);
+            $(".result").text("Rock Breaks Scissors. You Win!!");
             userScore++;
             $(".userScore").text(userScore);
-        } else if(userGuess === "rock" && computerGuess === "paper"){
-            console.log("You Lose");
+        } else if (userGuess === "rock" && computerGuess === "paper") {
+            $(".pickLeft").attr("src", leftRock);
+            $(".pickRight").attr("src", rightPaper);
+            $(".result").text("Rock is Covered By Paper. You Lose!!");
             computerScore++;
             $(".computerScore").text(computerScore);
-        } else if(userGuess === "paper" && computerGuess === "rock"){
-            console.log("You Win");
+        } else if (userGuess === "paper" && computerGuess === "rock") {
+            $(".pickLeft").attr("src", leftPaper);
+            $(".pickRight").attr("src", rightRock);
+            $(".result").text("Paper Covers Rock. You Win!!");
             userScore++;
             $(".userScore").text(userScore);
-        } else if(userGuess === "paper" && computerGuess === "scissors"){
-            console.log("You Lose");
+        } else if (userGuess === "paper" && computerGuess === "scissors") {
+            $(".pickLeft").attr("src", leftPaper);
+            $(".pickRight").attr("src", rightScissors);
+            $(".result").text("Paper is Cut By Scissors. You Lose!!");
             computerScore++;
             $(".computerScore").text(computerScore);
-        } else if(userGuess === "scissors" && computerGuess === "paper"){
-            console.log("You Win");
+        } else if (userGuess === "scissors" && computerGuess === "paper") {
+            $(".pickLeft").attr("src", leftScissors);
+            $(".pickRight").attr("src", rightPaper);
+            $(".result").text("Scissors Cuts Paper. You Win!!");
             userScore++;
             $(".userScore").text(userScore);
-        } else if(userGuess === "scissors" && computerGuess === "rock"){
-            console.log("You Lose");
+        } else if (userGuess === "scissors" && computerGuess === "rock") {
+            $(".pickLeft").attr("src", leftScissors);
+            $(".pickRight").attr("src", rightRock);
+            $(".result").text("Scissors is Smashed By Rock. You Lose!!");
             computerScore++;
             $(".computerScore").text(computerScore);
+        } else if (userGuess === "rock" && computerGuess === "rock") {
+            $(".pickLeft").attr("src", leftRock);
+            $(".pickRight").attr("src", rightRock);
+            $(".result").text("It's A Draw. Shoot Again!!");
+        } else if (userGuess === "paper" && computerGuess === "paper") {
+            $(".pickLeft").attr("src", leftPaper);
+            $(".pickRight").attr("src", rightPaper);
+            $(".result").text("It's A Draw. Shoot Again!!");
         } else {
-                console.log("It's a Draw");
+            $(".pickLeft").attr("src", leftScissors);
+            $(".pickRight").attr("src", rightScissors);
+            $(".result").text("It's A Draw. Shoot Again!!");
         }
-
     });
 
-    function getComputerChoice(){
+    function getComputerChoice() {
         const choices = ["rock", "paper", "scissors"];
-        let randomNumber = Math.floor(Math.random()*3);
+        let randomNumber = Math.floor(Math.random() * 3);
         computerGuess = choices[randomNumber];
     }
 
+    $(".front").on("click", function () {
+        $(".card").flip('toggle');
+    });
 
 });
